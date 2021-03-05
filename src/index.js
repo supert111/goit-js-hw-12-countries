fefe35b711d353b338f48f5b2a561300d94a5620
supert111 <31413087+supert111@users.noreply.github.com>
2021-03-06 00:54:18 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import './styles.css';
+//import './styles.css';
+
+//{name}
+fetch('https://restcountries.eu/rest/v2/name/Colombia')
+.then(response => {
+   // console.log(response.json());
+
+    return response.json();
+  })
+  .then(country => {
+      console.log(country);
+  })
+  .catch(error => {
+      console.log(error);
+  });
