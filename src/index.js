cbdd4ecc5c4a21b6b068efd68ad4cb10e2878f20
supert111 <sportsmen@bigmir.net>
2021-03-06 17:11:40 +0200

    settings

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-//import './styles.css';
+import './styles.css';
+import './js/theme';
 
-//{name}
-fetch('https://restcountries.eu/rest/v2/name/Colombia')
-.then(response => {
-   // console.log(response.json());
-
-    return response.json();
-  })
-  .then(country => {
-      console.log(country);
-  })
-  .catch(error => {
-      console.log(error);
-  });
