7dcd72df920085d9d2699d89aab84dacba2a8163
supert111 <sportsmen@bigmir.net>
2021-03-06 23:45:54 +0200

    settings pnotify

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import './styles.css';
 import './js/theme';
+
+
 import { alert, defaultModules } from '.././node_modules/@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '.././node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import { defaults } from '@pnotify/core';
 
+
+import '@pnotify/core/dist/Material.css';
+// import "@pnotify/core/dist/PNotify.css";
+// import "@pnotify/core/dist/BrightTheme.css";
+
+
+
+
+defaults.styling = 'material';
+defaults.icons = 'material';
 
  
 defaultModules.set(PNotifyMobile, {});
