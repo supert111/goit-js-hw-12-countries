a2d0711bbdb80aff636f2fa7cdd43f30393ba415
supert111 <sportsmen@bigmir.net>
2021-03-06 22:53:57 +0200

    settings css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
 import './js/theme';
-import { alert, defaultModules } from 'node_modules/@pnotify/core/dist/PNotify.js';
-import * as PNotifyMobile from 'node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import { alert, defaultModules } from '.././node_modules/@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '.././node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
 
 
  
