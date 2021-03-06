6a415fc19750b7b7381dad1184051a4058cfb3ee
supert111 <sportsmen@bigmir.net>
2021-03-06 21:58:27 +0200

    ttu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './styles.css';
 import './js/theme';
+import { alert, defaultModules } from 'node_modules/@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from 'node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
 
+
+ 
+defaultModules.set(PNotifyMobile, {});
+
+alert({
+  text: 'Notice me, senpai!'
+});
